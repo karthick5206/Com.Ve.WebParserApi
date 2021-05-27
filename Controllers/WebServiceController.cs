033e713c6b73271fdfde10f01d1d6736a2221451
karthik5206 <47052861+karthik5206@users.noreply.github.com>
2021-05-27 15:52:16 +0530

    Add project files.

---
+++ Controllers/WebServiceController.cs
@@ -0,0 +1,131 @@
+﻿using Com.Ve.ServerDataReceiver.RavenDB;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Com.Ve.WebParserApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WebServiceController : ControllerBase
+    {
+        private void Log(string log, LogType logType)
+        {
+            RavenDbConnector.Add(new LogData { Log = log, LogType = logType });
+        }
+
+        [HttpGet("ChatResponse")]
+        public string ChatResponse(String IMEI, String Reply)
+        {
+            try
+            {
+                Log("Chat Response", LogType.Info);
+                Log("Received Message IMEI : " + IMEI, LogType.Info);
+                Log("Received Message Reply : " + Reply, LogType.Info);
+            }
+            catch (Exception ex)
+            {
+                Log(ex.StackTrace, LogType.Error);
+            }
+            string XML = "<http://tempuri.org/>SUCCESS0</http://tempuri.org/>";
+            return XML;
+        }
+
+        [HttpGet("GetCommands")]
+
+        public string GetCommands(string IMEI)
+        {
+            try
+            {
+                Log("GetCommands", LogType.Info);
+                Log("Received Message IMEI : " + IMEI, LogType.Info);
+            }
+            catch (Exception ex)
+            {
+                Log(ex.StackTrace, LogType.Error);
+            }
+            string XML = "<http://tempuri.org/></http://tempuri.org/>";
+            return XML;
+            //return new JavaScriptSerializer().Serialize("");
+        }
+        [HttpGet("FirmwareUpdated")]
+        private string FirmwareUpdated(String IMEI, String FirmwareId)
+        {
+            try
+            {
+                Log("FirmwareUpdated", LogType.Info);
+                Log("Received Message IMEI : " + IMEI, LogType.Info);
+                Log("Received Message FirmwareId : " + FirmwareId, LogType.Info);
+            }
+            catch (Exception ex)
+            {
+                Log(ex.StackTrace, LogType.Error);
+            }
+            string XML = "<http://tempuri.org/>SUCCESS0</http://tempuri.org/>";
+            return XML;
+            //return new JavaScriptSerializer().Serialize("SUCCESS0");
+        }
+
+        [HttpGet("DeviceStatus")]
+        public string DeviceStatus(String IMEI, String GpsStatus)
+        {
+            try
+            {
+                Log("DeviceStatus", LogType.Info);
+                Log("Received Message IMEI : " + IMEI, LogType.Info);
+                Log("Received Message GpsStatus : " + GpsStatus, LogType.Info);
+            }
+            catch (Exception ex)
+            {
+                Log(ex.StackTrace, LogType.Error);
+            }
+            string XML = "<http://tempuri.org/>SUCCESS0</http://tempuri.org/>";
+            return XML;
+            // return new JavaScriptSerializer().Serialize("SUCCESS0");
+        }
+
+        [HttpGet("RawTripLog")]
+        public string RawTripLog(String IMEI, String TripLogData)
+        {
+            try
+            {
+                Log("RawTripLog", LogType.Info);
+                Log("Received Message IMEI : " + IMEI,
+                    LogType.Info);
+                Log("Received Message TripLogData : " + TripLogData, LogType.Info);
+                var receivedMessage = IMEI + "/" + TripLogData;
+                if (!string.IsNullOrEmpty(receivedMessage) && receivedMessage.Length > 1)
+                {
+                    Log("Received Message : " + receivedMessage, LogType.Info);
+                    var request = (HttpWebRequest)WebRequest.Create("http://localhost:4580/Service.ashx/RawTripLog");
+
+                    //LogFile.Info("Address : " + request.Address);
+
+                    var data = Encoding.ASCII.GetBytes(receivedMessage);
+
+                    request.Method = "POST";
+                    request.ContentType = "application/x-www-form-urlencoded";
+                    request.ContentLength = data.Length;
+
+                    using (var stream = request.GetRequestStream())
+                    {
+                        stream.Write(data, 0, data.Length);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Log(ex.StackTrace, LogType.Error);
+            }
+            string XML = "<http://tempuri.org/>SUCCESS</http://tempuri.org/>";
+            return XML;
+            //return new JavaScriptSerializer().Serialize("SUCCESS");
+        }
+    }
+}
---
+++ RavenDB/RavenDbConnector.cs
@@ -0,0 +1,134 @@
+﻿using Raven.Client.Documents;
+using Raven.Client.Documents.Indexes;
+using Raven.Client.Documents.Operations.Indexes;
+using Raven.Client.Documents.Session;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Com.Ve.ServerDataReceiver.RavenDB
+{
+    public static class RavenDbConnector
+    {
+        static RavenDbConnector()
+        {
+            try
+            {
+                Console.WriteLine($"Initializing Raven Db..");
+                using (IDocumentSession session = DocumentStoreHolder.Store.OpenSession())
+                {
+                    Console.WriteLine($"Connected Raven Db.. Opened Session...");
+
+                    string[] indexNames = DocumentStoreHolder.Store.Maintenance.Send(new GetIndexNamesOperation(0, int.MaxValue));
+                    Console.WriteLine($"Index count...{indexNames?.Length}");
+
+                    if (indexNames?.Any(i => i == nameof(GpsDataDateTimeIndex)) ?? false)
+                        new GpsDataDateTimeIndex().Execute(DocumentStoreHolder.Store);
+
+                    Console.WriteLine($"Index inserted...{nameof(GpsDataDateTimeIndex)}");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error while initializing ...{ex}");
+            }
+        }
+
+        public static void Add(GpsData gpsData)
+        {
+            try
+            {
+                Console.WriteLine($"New data received - {DateTime.Now}");
+
+                using (IDocumentSession session = DocumentStoreHolder.Store.OpenSession())
+                {
+                    session.Store(gpsData);
+
+                    session.SaveChanges();
+                }
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine($"Error while adding ...{ex}");
+            }
+        }
+
+        public static void Add(LogData gpsData)
+        {
+            try
+            {
+                Console.WriteLine($"New log received - {DateTime.Now}");
+
+                using (IDocumentSession session = DocumentStoreHolder.Store.OpenSession())
+                {
+                    session.Store(gpsData);
+
+                    session.SaveChanges();
+                }
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine($"Error while adding ...{ex}");
+            }
+        }
+    }
+
+    public class LogData
+    {
+        public string Log { get; set; }
+        public LogType LogType { get; set; }
+        public DateTime DateTime => DateTime.Now;
+    }
+
+    public enum LogType
+    {
+        Info,
+        Error
+    }
+
+    public class GpsDataDateTimeIndex : AbstractIndexCreationTask<GpsData>
+    {
+        public GpsDataDateTimeIndex()
+        {
+            Map = gpsData =>
+              from o in gpsData
+              select new
+              {
+                  o.Data,
+                  o.DateTime,
+              };
+        }
+    }
+
+    public static class DocumentStoreHolder
+    {
+        private static X509Certificate2 clientCertificate = new X509Certificate2(@$"{Directory.GetCurrentDirectory()}/RavenDB/Pfix/free.vibhav.client.certificate.pfx");
+
+        private static readonly Lazy<IDocumentStore> LazyStore =
+            new Lazy<IDocumentStore>(() =>
+            {
+                var store = new DocumentStore
+                {
+                    Certificate = clientCertificate,
+                    Urls = new[] { "https://a.free.vibhav.ravendb.cloud" },
+                    Database = "GPSRawData"
+                };
+
+                return store.Initialize();
+            });
+
+        public static IDocumentStore Store => LazyStore.Value;
+    }
+    public class GpsData
+    {
+        public string Id { get; set; }
+        public string Data { get; set; }
+        public DateTime DateTime => DateTime.Now;
+    }
+}
