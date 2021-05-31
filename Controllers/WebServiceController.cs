c5731c0cac8b981345770f0de94b507fda6286f4
karthik5206 <47052861+karthik5206@users.noreply.github.com>
2021-05-31 23:38:55 +0530

    added xml tag

--- Controllers/WebServiceController.cs
+++ Controllers/WebServiceController.cs
@@ -20,9 +20,9 @@ namespace Com.Ve.WebParserApi.Controllers
     [ApiController]
     public class WebServiceController : ControllerBase
     {
-        private const string ReplySuccess = "<string xmlns=\"http://tempuri.org/\">\"SUCCESS\"</string>";
-        private const string ReplyEmptySuccess = "<string xmlns=\"http://tempuri.org/\">\"\"</string>";
-        private const string ReplySuccess0 = "<string xmlns=\"http://tempuri.org/\">\"SUCCESS0\"</string>";
+        private const string ReplySuccess = "<?xml version=\"1.0\" encoding=\"utf-8\"?>\n<string xmlns=\"http://tempuri.org/\">\"SUCCESS\"</string>";
+        private const string ReplyEmptySuccess = "<?xml version=\"1.0\" encoding=\"utf-8\"?>\n<string xmlns=\"http://tempuri.org/\">\"\"</string>";
+        private const string ReplySuccess0 = "<?xml version=\"1.0\" encoding=\"utf-8\"?>\n<string xmlns=\"http://tempuri.org/\">\"SUCCESS0\"</string>";
         private const string ContentType = "text/xml; charset=utf-8";
         private void Log(string log, LogType logType)
         {
